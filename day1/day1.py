825486b2a1fea6b8b32aae96a65cd5ab803f437d
Marin260 <marin.martuslovic@gmail.com>
2021-12-03 22:31:43 +0100

    day one complete

--- day1/day1.py
+++ day1/day1.py
@@ -1,15 +1,26 @@
+# Part one
 with open('input.txt') as f:
     scans = f.readlines()
 
 num_of_depth_changes = 0
 scans[0] = int(scans[0].rstrip("\n")) # first skiped value
 
-for i in range(len(scans[1:])): # go throught the list
+for i in range(len(scans[1:])): # go through the list
     scans[i+1] = int(scans[i+1].rstrip("\n")) # remve \n and trnsform to int
     if scans[i+1] > scans[i]: # if the number b4 was bigger add 1 to cout
         num_of_depth_changes +=1
 
 
 print(num_of_depth_changes)   
+# Part two
 
-#print(scans)
+num_of_depth_changes = 0
+tmpSum = scans[0] + scans[1] + scans[2]
+
+for i in range(len(scans[1:-2])): # go through the list
+    curSum = scans[i+1] + scans[i+2] + scans[i+3] # sum 3 consecutive elements
+    if curSum > tmpSum: # if the sum b4 was bigger add 1 to cout
+        num_of_depth_changes +=1
+    tmpSum = curSum
+print(num_of_depth_changes)
+
