811d8f9b02aeee879dc5aefeea368b4752c18b4f
Marin260 <marin.martuslovic@gmail.com>
2021-12-06 21:20:59 +0100

    added some comments for better future understanding

--- day6/day6.py
+++ day6/day6.py
@@ -2,7 +2,7 @@ from collections import Counter
 with open('day6/input.txt') as f: # load file
     scans = f.readlines()
 
-
+# pretty sure i can improve the logic in the main for loop and skip and avoid everything from line 9-17
 
 scans = list(map(int, scans[0].split(',')))
 # transform input to dict where keys are days and values reapeating instances of that day
@@ -13,21 +13,19 @@ for i in range(9): # add the keys of the days that do not exist
         listOfFishDays[i] = listOfFishDays[i]
     except KeyError:
         listOfFishDays[i] = 0
-listOfFishDays = dict(sorted(listOfFishDays.items())) # no need for sorting
+listOfFishDays = dict(sorted(listOfFishDays.items())) # no need for this at all, just for a later visual rep
 
-for i in range(256): 
+for i in range(256): # main for loop logic 
     listOfFishDays['tmp'] = listOfFishDays[8] # mem of reset fish
     for k in range(9): # move all the fishies with the same day closer to 0
         if k == 0: 
             listOfFishDays[6] += listOfFishDays['tmp'] # reset the new fishies
             listOfFishDays['tmp'] = listOfFishDays[0] # newBorns
-           
         if k == 8:
             listOfFishDays[k] = listOfFishDays['tmp'] # tmp value for last day
             break
         
         listOfFishDays[k] = listOfFishDays[k+1]
         
-
 values = sum(listOfFishDays.values()) # sum all the values
 print(values)
